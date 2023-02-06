be86b02fabede38eb3fc268a77d64e269ce221e3
Bruno Leonardo <2295132@cegepoutaouais.qc.ca>
2023-02-06 11:07:20 -0600

    version finale 06/02/2023

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 class Employe {
-    constructor(nom, prenom, age, dateNaissance) {
+
+    constructor(nom = "Nom", prenom ="Prenom", age = 20, dateNaissance = "2000-02-02") {
         this.nom = nom;
         this.prenom = prenom;
         this.age = age;
@@ -7,19 +8,22 @@ class Employe {
     }
 
     toString() {
-        return this.prenom + " " + this.nom + "\n" + this.dateNaissance + " " + this.age + "ans " + this.dateNaissance;
+        return this.prenom + " " + this.nom + "\n" + this.dateNaissance + " " + this.age + " ans."
     }
 }
 
 function creerEmployer(){
+
+    //const emp2 = new Employe();
+    //document.getElementById("employe").innerText = emp2
+
     let nom = document.getElementById("nom").value;
     let prenom = document.getElementById("prenom").value;
     let age = document.getElementById("age").value;
-    let dateNaissance = document.getElementById("date").value;
+    let date = document.getElementById("date").value;
 
-    const employe = new Employe(nom,prenom,age,dateNaissance)
-    let nweSpan = document.createElement("p");
+    const employe = new Employe(nom, prenom, age, date);
+    let newSpan = document.createElement("p");
     newSpan.innerText = employe;
     document.getElementById("employe").append(newSpan);
-
 }
