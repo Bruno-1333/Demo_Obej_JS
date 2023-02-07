7901b8a9594f19a59cd42651af2c3b39c6b847f6
Bruno Leonardo <2295132@cegepoutaouais.qc.ca>
2023-02-06 22:50:45 -0600

    version finale 06/02/2023 comentado

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 class Employe {
+    // Construtor da classe Employe
+    // Os valores padrão são definidos para os atributos
 
     constructor(nom = "Nom", prenom ="Prenom", age = 20, dateNaissance = "2000-02-02") {
         this.nom = nom;
@@ -7,23 +9,27 @@ class Employe {
         this.dateNaissance = dateNaissance;
     }
 
+    // Método toString retorna uma string formatada com os atributos do objeto
     toString() {
         return this.prenom + " " + this.nom + "\n" + this.dateNaissance + " " + this.age + " ans."
     }
 }
-
+// Função creerEmployer cria um novo objeto Employe
+// com base em valores inseridos no HTML
 function creerEmployer(){
 
-    //const emp2 = new Employe();
-    //document.getElementById("employe").innerText = emp2
-
+    // Obtém os valores do formulário HTML usando document.getElementById e .value
     let nom = document.getElementById("nom").value;
     let prenom = document.getElementById("prenom").value;
     let age = document.getElementById("age").value;
     let date = document.getElementById("date").value;
-
+    // Cria um novo objeto Employe com os valores obtidos do formulário
     const employe = new Employe(nom, prenom, age, date);
+
+    // Cria uma nova tag <p> e define seu conteúdo como a string retornada pelo método toString
     let newSpan = document.createElement("p");
     newSpan.innerText = employe;
+
+    // Adiciona a nova tag <p> ao elemento HTML com o ID "employe"
     document.getElementById("employe").append(newSpan);
 }
